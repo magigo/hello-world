3183e7069c17c98a0e1052d3ca7182572144556b
纪路 <magi-go@126.com>
2018-08-24 18:31:57 +0800

    Update index.php

--- www/index.php
+++ www/index.php
@@ -17,8 +17,8 @@
 </head>
 <body>
 	<img id="logo" src="logo.png" />
-	<h1><?php echo "Hello ".($_ENV["NAME"]?$_ENV["NAME"]:"world")."!"; ?></h1>
-	<?php if($_ENV["HOSTNAME"]) {?><h3>T2 My hostname is <?php echo $_ENV["HOSTNAME"]; ?></h3><?php } ?>
+	<h1><?php echo "T3 Hello ".($_ENV["NAME"]?$_ENV["NAME"]:"world")."!"; ?></h1>
+	<?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?></h3><?php } ?>
 	<?php
 	$links = [];
 	foreach($_ENV as $key => $value) {
